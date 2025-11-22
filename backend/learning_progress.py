1f29d71a886ee8638fc7b9c15d3fd9889cafe6ea
Code-Ph0enix <eeshanyajoshi@gmail.com>
2025-11-23 00:02:42 +0530

    bakchodi

--- backend/learning_progress.py
+++ backend/learning_progress.py
@@ -9,16 +9,20 @@ class PyObjectId(ObjectId):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
-
+    
     @classmethod
     def validate(cls, v):
         if not ObjectId.is_valid(v):
-            raise ValueError("Invalid objectid")
+            raise ValueError("Invalid ObjectId")
         return ObjectId(v)
 
     @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
+    def __get_pydantic_json_schema__(
+        cls, core_schema, handler: GetJsonSchemaHandler
+    ) -> JsonSchemaValue:
+        json_schema = handler(core_schema)
+        json_schema.update(type="string")
+        return json_schema
 
 class CourseProgress(BaseModel):
     course_id: str
